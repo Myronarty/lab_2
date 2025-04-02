9541c17931c36e4a25225fe81ad14ffffb4a3f70
Myronarty <ahamalianmyron@gmail.com>
2025-04-02 19:51:34 +0300

    1st program

--- lab_2/Program.cs
+++ lab_2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int a = input.Length;
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(input[i]);
+                }
+            }
+        }
+    }
+}
